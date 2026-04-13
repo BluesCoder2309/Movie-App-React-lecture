8d308b486c4fced1ea077c431b199221013c72aa
Utkarsh Singh <singh.utkarsh2309@gmail.com>
2026-04-13 19:36:56 +0530

    used css modules to style the navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,66 +1,67 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
+import styles from "./Navbar.module.css";
 
-const Nav = styled.div`
-  width: 100%;
-  height: 70px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-  background: linear-gradient(170deg, #1bc059, #0d47a1);
-  
-`;
+// const Nav = styled.div`
+//   width: 100%;
+//   height: 70px;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   position: relative;
+//   background: linear-gradient(170deg, #1bc059, #0d47a1);
 
-const Title = styled.div`
-  font-size: 3rem;
-  color: #fff;
-  font-weight: 600;
-  font-wamily: Montserrat, sans-serif;
-  text-transform: uppercase;
-  margin-left: 20px;
-  &:hover {
-    color: black;
-    cursor : pointer ;
-  }
-`;
+// `;
 
-const CartIconContainer = styled.div`
-  position: relative;
-  cursor: pointer;
-`;
+// const Title = styled.div`
+//   font-size: 3rem;
+//   color: #fff;
+//   font-weight: 600;
+//   font-family: Montserrat, sans-serif;
+//   text-transform: uppercase;
+//   margin-left: 20px;
+//   &:hover {
+//     color: black;
+//     cursor : pointer ;
+//   }
+// `;
 
-const CartIcon = styled.img`
-  height: 48px;
-  margin-right: 20px;
-  margin-top: 10px;
-`;
+// const CartIconContainer = styled.div`
+//   position: relative;
+//   cursor: pointer;
+// `;
 
-const CartCount = styled.span`
-  background-color: ${(props) => props.color};
-  border-radius: 50%;
-  padding: 4px 8px;
-  position: absolute;
-  right: 10px;
-  top: 5px;
-  font-size: 12px;
-  visibility: ${(props) => (props.show ? "visible" : "hidden")};
-`;
+// const CartIcon = styled.img`
+//   height: 48px;
+//   margin-right: 20px;
+//   margin-top: 10px;
+// `;
+
+// const CartCount = styled.span`
+//   background-color: ${(props) => props.color};
+//   border-radius: 50%;
+//   padding: 4px 8px;
+//   position: absolute;
+//   right: 10px;
+//   top: 5px;
+//   font-size: 12px;
+//   visibility: ${(props) => (props.show ? "visible" : "hidden")};
+// `;
 class Navbar extends Component {
   render() {
     return (
-      <Nav>
-        <Title>Movie-App</Title>
-        <CartIconContainer>
-          <CartIcon
+      <div className={styles.nav}>
+        <div className={styles.title}>Movie-App</div>
+        <div className={styles.cartIconContainer}>
+          <img className={styles.cartIcon}
             src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
             alt="cart-icon"
           />
-          <CartCount color="skyblue" show={true}>
+          <span className={styles.cartCount}>
             3
-          </CartCount>
-        </CartIconContainer>
-      </Nav>
+          </span>
+        </div>
+      </div>
     );
   }
 }
